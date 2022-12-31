8781259d7d0783a1f18d84f57f51e6dc4946bb24
NicolaPufnei <nicolapufnei@gmail.com>
2022-12-31 12:43:04 +0200

    v7

--- main.js
+++ main.js
@@ -21,6 +21,22 @@ const moon_button = document.querySelector('.moon-button')
 const mars_button = document.querySelector('.mars-button')
 const europa_button = document.querySelector('.europa-button')
 const titan_button = document.querySelector('.titan-button')
+                // photos constants
+const img1 = document.querySelector('.img1')
+const img2 = document.querySelector('.img2')
+const img3 = document.querySelector('.img3')
+const img4 = document.querySelector('.img4')
+                // text constant
+const text1 = document.querySelector('.commander')
+const text2 = document.querySelector('.mission-specialist')
+const text3 = document.querySelector('.pilot')
+const text4 = document.querySelector('.flight-engineer')
+                // buttons constants
+const buton1 = document.querySelector('.button1')
+const buton2 = document.querySelector('.button2')
+const buton3 = document.querySelector('.button3')
+const buton4 = document.querySelector('.button4')
+
 
 // Hamburger menu function
 
@@ -104,4 +120,80 @@ function titanButton() {
     mars_button.classList.replace('active' , 'inactive');
     titan_button.classList.replace('inactive' , 'active');
     europa_button.classList.replace('active' , 'inactive');
+}
+
+// Crew button 1
+
+function button1() {
+    img1.classList.replace('inactive' , 'active');
+    img2.classList.replace('active' , 'inactive');
+    img3.classList.replace('active' , 'inactive');
+    img4.classList.replace('active' , 'inactive');
+
+    text1.classList.replace('inactive' , 'active');
+    text2.classList.replace('active' , 'inactive');
+    text3.classList.replace('active' , 'inactive');
+    text4.classList.replace('active' , 'inactive');
+
+    buton1.classList.replace('inactive' , 'active');
+    buton2.classList.replace('active' , 'inactive');
+    buton3.classList.replace('active' , 'inactive');
+    buton4.classList.replace('active' , 'inactive');
+}
+
+// Crew button 2
+
+function button2() {
+    img1.classList.replace('active' , 'inactive');
+    img2.classList.replace('inactive' , 'active');
+    img3.classList.replace('active' , 'inactive');
+    img4.classList.replace('active' , 'inactive');
+
+    text1.classList.replace('active' , 'inactive');
+    text2.classList.replace('inactive' , 'active');
+    text3.classList.replace('active' , 'inactive');
+    text4.classList.replace('active' , 'inactive');
+
+    buton1.classList.replace('active' , 'inactive');
+    buton2.classList.replace('inactive' , 'active');
+    buton3.classList.replace('active' , 'inactive');
+    buton4.classList.replace('active' , 'inactive');
+}
+
+// Crew button 3
+
+function button3() {
+    img1.classList.replace('active' , 'inactive');
+    img2.classList.replace('active' , 'inactive');
+    img3.classList.replace('inactive' , 'active');
+    img4.classList.replace('active' , 'inactive');
+
+    text1.classList.replace('active' , 'inactive');
+    text2.classList.replace('active' , 'inactive');
+    text3.classList.replace('inactive' , 'active');
+    text4.classList.replace('active' , 'inactive');
+
+    buton1.classList.replace('active' , 'inactive');
+    buton2.classList.replace('active' , 'inactive');
+    buton3.classList.replace('inactive' , 'active');
+    buton4.classList.replace('active' , 'inactive');
+}
+
+// Crew button 4
+
+function button4() {
+    img1.classList.replace('active' , 'inactive');
+    img2.classList.replace('active' , 'inactive');
+    img3.classList.replace('active' , 'inactive');
+    img4.classList.replace('inactive' , 'active');
+
+    text1.classList.replace('active' , 'inactive');
+    text2.classList.replace('active' , 'inactive');
+    text3.classList.replace('active' , 'inactive');
+    text4.classList.replace('inactive' , 'active');
+
+    buton1.classList.replace('active' , 'inactive');
+    buton2.classList.replace('active' , 'inactive');
+    buton3.classList.replace('active' , 'inactive');
+    buton4.classList.replace('inactive' , 'active');
 }
